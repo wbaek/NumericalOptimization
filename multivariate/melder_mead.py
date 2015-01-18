9e2dd227835a9a8f91d53c4dc2ef9929fb021c9c
wbaek <wbaek.personal@gmail.com>
2015-01-18 22:21:45 +0900

    add example number of params = 3

--- multivariate/melder_mead.py
+++ multivariate/melder_mead.py
@@ -2,7 +2,7 @@
 def _verbose(i, params, scores):
     params_str = ', '.join(['(%s)'%(','.join(['%.2f'%p for p in param])) for param in params])
     scores_str = ', '.join(['%.2f'%s for s in scores])
-    print 'iter=%03d, params=%s, scores%s'%(i, params_str, scores_str)
+    print 'iter=%03d, params=[%s], scores=[%s]'%(i, params_str, scores_str)
 
 def minimize(function, initial, alpha=1.0, beta=2.0, gamma=0.5, epsilon=1e-5, repeat=int(1e6), verbose=False):
     ''' melder mead method
@@ -10,20 +10,25 @@ def minimize(function, initial, alpha=1.0, beta=2.0, gamma=0.5, epsilon=1e-5, re
             [(0.0,0.0), (1.0,1.0), (0.0,1.0)] )
     (0.30000068443533018, 1.3000025269138833)
 
+    >>> minimize( lambda x: abs(x[0]-0.3) + abs(x[1]-1.3) + abs(x[2]+0.7) ,\
+            [(0.0,0.0,0.0), (1.0,1.0,1.0), (0.0,1.5,0.0), (1.5,2.5,0.0)] )
+    (0.30000098578208223, 1.3000002242389832, -0.70000285189454658)
+
     >>> minimize( lambda x: (x[0]-0.3)**2 + (x[1]-1.3)**2 + 3.0 ,\
             [(0.3,0.0), (1.0,1.0), (0.0,1.3)], repeat=10, verbose=True )
-    iter=000, params=(0.00,1.30), (1.00,1.00), (0.30,0.00), scores3.09, 3.58, 4.69
-    iter=001, params=(0.00,1.30), (0.60,1.72), (1.00,1.00), scores3.09, 3.27, 3.58
-    iter=002, params=(0.00,1.30), (0.65,1.26), (0.60,1.72), scores3.09, 3.12, 3.27
-    iter=003, params=(0.46,1.50), (0.00,1.30), (0.65,1.26), scores3.07, 3.09, 3.12
-    iter=004, params=(0.44,1.33), (0.46,1.50), (0.00,1.30), scores3.02, 3.07, 3.09
-    iter=005, params=(0.23,1.36), (0.44,1.33), (0.46,1.50), scores3.01, 3.02, 3.07
-    iter=006, params=(0.27,1.26), (0.23,1.36), (0.44,1.33), scores3.00, 3.01, 3.02
-    iter=007, params=(0.27,1.26), (0.34,1.32), (0.23,1.36), scores3.00, 3.00, 3.01
-    iter=008, params=(0.27,1.32), (0.27,1.26), (0.34,1.32), scores3.00, 3.00, 3.00
-    iter=009, params=(0.31,1.31), (0.27,1.32), (0.27,1.26), scores3.00, 3.00, 3.00
+    iter=000, params=[(0.00,1.30), (1.00,1.00), (0.30,0.00)], scores=[3.09, 3.58, 4.69]
+    iter=001, params=[(0.00,1.30), (0.60,1.72), (1.00,1.00)], scores=[3.09, 3.27, 3.58]
+    iter=002, params=[(0.00,1.30), (0.65,1.26), (0.60,1.72)], scores=[3.09, 3.12, 3.27]
+    iter=003, params=[(0.46,1.50), (0.00,1.30), (0.65,1.26)], scores=[3.07, 3.09, 3.12]
+    iter=004, params=[(0.44,1.33), (0.46,1.50), (0.00,1.30)], scores=[3.02, 3.07, 3.09]
+    iter=005, params=[(0.23,1.36), (0.44,1.33), (0.46,1.50)], scores=[3.01, 3.02, 3.07]
+    iter=006, params=[(0.27,1.26), (0.23,1.36), (0.44,1.33)], scores=[3.00, 3.01, 3.02]
+    iter=007, params=[(0.27,1.26), (0.34,1.32), (0.23,1.36)], scores=[3.00, 3.00, 3.01]
+    iter=008, params=[(0.27,1.32), (0.27,1.26), (0.34,1.32)], scores=[3.00, 3.00, 3.00]
+    iter=009, params=[(0.31,1.31), (0.27,1.32), (0.27,1.26)], scores=[3.00, 3.00, 3.00]
     (0.30558776855468739, 1.3068706512451174)
     '''
+
     import numpy
     def center_of_params(params):
         return tuple(numpy.average(numpy.array(params[:-1]).transpose(), 1))
