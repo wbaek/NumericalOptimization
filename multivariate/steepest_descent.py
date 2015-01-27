dfd87f7075237cc1b497b8cea6ccbf73c0747046
wbaek <wbaek.personal@gmail.com>
2015-01-28 01:24:09 +0900

    add conjugate gradient

--- multivariate/steepest_descent.py
+++ multivariate/steepest_descent.py
@@ -6,23 +6,34 @@ def _verbose(i, param, direction, step_length, score):
     print 'iter=%03d, params=%s, direction=%s, step_length=%.5f, scores=%s'%(i, params_str, direction_str, step_length, scores_str)
 
 import numpy
-def check_wolfe_condition(score_pt, score_alpha_pt, gradient_pt, gradient_alpha_pt, alpha, direction,
-        constant1=0.6, constant2=0.8):
+def wolfe_condition(score_pt, score_alpha_pt, gradient_pt, gradient_alpha_pt, alpha, direction,
+        constant1=0.6, constant2=0.9):
     if score_alpha_pt - score_pt <= constant1 * alpha * numpy.dot(gradient_pt, direction) \
     and numpy.dot(gradient_alpha_pt, direction) >= constant2 * numpy.dot(gradient_pt, direction):
         return True
     else:
         return False
 
+def strong_wolfe_condition(score_pt, score_alpha_pt, gradient_pt, gradient_alpha_pt, alpha, direction,
+        constant1=0.6, constant2=0.9):
+    if score_alpha_pt - score_pt <= constant1 * alpha * numpy.dot(gradient_pt, direction) \
+    and abs(numpy.dot(gradient_alpha_pt, direction)) <= - constant2 * numpy.dot(gradient_pt, direction):
+        return True
+    else:
+        return False
+
 def find_step_length(function, derivate, pt, direction,
-        tau=0.7, repeat=int(1e4), initial_length=1.0):
+        tau=0.8, repeat=int(1e2), initial_length=1.0, check=wolfe_condition):
     score_at_pt = function(pt)
     gradient_at_pt = derivate(pt)
 
-    alpha = 0.0
+    length_of_direction = numpy.linalg.norm(direction, 2)
+    direction /= length_of_direction
+
+    alpha = 1.0
     start_pt = pt
-    last_pt = pt + direction * initial_length
-    for i in range(100):
+    last_pt = pt + direction * initial_length * length_of_direction
+    for i in range(repeat):
         length = numpy.linalg.norm( last_pt - start_pt, 2 )
         pt1 = start_pt + direction * ( (1.0 - tau) * length )
         pt2 = start_pt + direction * ( (tau) * length )
@@ -30,11 +41,13 @@ def find_step_length(function, derivate, pt, direction,
         score_pt2 = function( pt2 )
 
         # wolfe condition
-        alpha = (1.0 - tau) * length
-        if check_wolfe_condition(score_at_pt, score_pt1, gradient_at_pt, derivate(pt1), alpha, direction):
+        _ = (1.0 - tau) * length
+        if check(score_at_pt, score_pt1, gradient_at_pt, derivate(pt1), _, direction):
+            alpha = _
             break
-        alpha = (tau) * length
-        if check_wolfe_condition(score_at_pt, score_pt2, gradient_at_pt, derivate(pt2), alpha, direction):
+        _ = (tau) * length
+        if check(score_at_pt, score_pt2, gradient_at_pt, derivate(pt2), _, direction):
+            alpha = _
             break
 
         if score_pt1 >= score_pt2: start_pt = pt1
@@ -60,16 +73,16 @@ def minimize(function, derivate, initial, epsilon=1e-6, repeat=int(1e4), verbose
             lambda x: (x[0]-0.3)**2 + (x[1]-1.3)**2 + 3.0, \
             lambda x: (2*x[0] - 0.6, 2*x[1] -2.6), \
             (0.0, 0.0), repeat=10, verbose=True)]
-    iter=000, params=(0.00,0.00), direction=(0.22,0.97), step_length=0.30000, scores=4.78
-    iter=001, params=(0.07,0.29), direction=(0.22,0.97), step_length=0.30000, scores=4.07
-    iter=002, params=(0.13,0.58), direction=(0.22,0.97), step_length=0.30000, scores=3.54
-    iter=003, params=(0.20,0.88), direction=(0.22,0.97), step_length=0.30000, scores=3.19
-    iter=004, params=(0.27,1.17), direction=(0.22,0.97), step_length=0.10290, scores=3.02
-    iter=005, params=(0.29,1.27), direction=(0.22,0.97), step_length=0.02471, scores=3.00
-    iter=006, params=(0.30,1.29), direction=(0.22,0.97), step_length=0.00415, scores=3.00
-    iter=007, params=(0.30,1.30), direction=(0.22,0.97), step_length=0.00142, scores=3.00
-    iter=008, params=(0.30,1.30), direction=(0.22,0.97), step_length=0.00070, scores=3.00
-    iter=009, params=(0.30,1.30), direction=(0.22,0.97), step_length=0.00017, scores=3.00
+    iter=000, params=(0.00,0.00), direction=(0.22,0.97), step_length=0.80000, scores=4.78
+    iter=001, params=(0.18,0.78), direction=(0.22,0.97), step_length=0.20000, scores=3.29
+    iter=002, params=(0.22,0.97), direction=(0.22,0.97), step_length=0.20000, scores=3.11
+    iter=003, params=(0.27,1.17), direction=(0.22,0.97), step_length=0.06554, scores=3.02
+    iter=004, params=(0.28,1.23), direction=(0.22,0.97), step_length=0.03355, scores=3.00
+    iter=005, params=(0.29,1.27), direction=(0.22,0.97), step_length=0.01718, scores=3.00
+    iter=006, params=(0.30,1.28), direction=(0.22,0.97), step_length=0.00880, scores=3.00
+    iter=007, params=(0.30,1.29), direction=(0.22,0.97), step_length=0.00450, scores=3.00
+    iter=008, params=(0.30,1.30), direction=(0.22,0.97), step_length=0.00231, scores=3.00
+    iter=009, params=(0.30,1.30), direction=(0.22,0.97), step_length=0.00118, scores=3.00
     ['0.30', '1.30']
     '''
 
